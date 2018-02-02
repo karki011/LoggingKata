ea463463a89aa09d428080f10776803cd9c1cfc3
Subash Karki <karki011@gmail.com>
2018-02-02 08:35:30 -0800

    Test unit working

--- LoggingKata.Test/ParserTest.cs
+++ LoggingKata.Test/ParserTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using LoggingKata;
 
 namespace LoggingKata.Test
 {
@@ -11,9 +12,32 @@ namespace LoggingKata.Test
     public class TacoParserTestFixture
     {
         [Test]
-        public void ShouldParseLine()
+        public void ShouldReturnNullForEmptyString()
         {
             //TODO: Complete ShouldParseLine
+            //Arrange
+            var line = "";
+            var parser = new TacoParser();
+
+            // Act
+            var result = parser.Parse(line);
+
+            // assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void ShouldParseLine()
+        {
+            //Arrange
+            var tacoParser = new TacoParser();
+            var tacoCoordinates = "-84.677017, 34.073638, Taco Bell Acwort... (Free trial * Add to Cart for a full POIinfo";
+
+            //Act
+            var resultTaco = tacoParser.Parse(tacoCoordinates);
+
+            //Assert
+            Assert.IsNotNull(resultTaco);
         }
     }
 }
